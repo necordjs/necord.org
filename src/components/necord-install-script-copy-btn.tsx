8cd6a2559fcd51fa20046380cd455444afb2a44a
SocketSomeone <socket.someone@gmail.com>
2025-05-17 11:57:31 +0300

    feat: add home page

---
+++ src/components/necord-install-script-copy-btn.tsx
@@ -0,0 +1,18 @@
+import { ScriptCopyBtn } from "@/components/magicui/script-copy-btn";
+
+export function NecordInstallScriptCopyBtn() {
+	const customCommandMap = {
+		npm: "npm install necord discord.js",
+		yarn: "yarn add necord discord.js",
+		pnpm: "pnpm add necord discord.js",
+	};
+	return (
+		<ScriptCopyBtn
+			showMultiplePackageOptions={false}
+			codeLanguage="shell"
+			lightTheme=""
+			darkTheme=""
+			commandMap={customCommandMap}
+		/>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,74 @@
 import React from 'react';
-import Layout from '@theme/Layout';
+import { NecordInstallScriptCopyBtn } from '../components/necord-install-script-copy-btn';
+import { Button } from '../components/ui/button';
+import Head from '@docusaurus/Head';
 
-export default function Hello() {
+export default function Home() {
+	const openLink = (url: string, onSite: boolean = false) => {
+		window.open(url, onSite ? '_self' : '_blank');
+	};
+
 	return (
-		<Layout title="Hello" description="Hello React Page">
-			<div
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					height: '50vh',
-					fontSize: '20px'
-				}}
-				className="text-red-500"
-			>
-				<p>
-					Edit <code>pages/helloReact.js</code> and save to reload.
+		<>
+			<Head>
+				<title>Necord - The best way to create Discord bots with NestJS</title>
+				<meta
+					name="description"
+					content="The best way to create Discord bots with NestJS"
+				/>
+			</Head>
+			<div className="w-4xl mx-auto flex flex-col items-center justify-center h-screen space-y-6">
+				<h1 className="text-7xl font-bold text-center text-primary">
+					The best way to create{' '}
+					<span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700">
+					Discord
+				</span>{' '}
+					bots with{' '}
+					<span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-700">
+					NestJS
+				</span>
+				</h1>
+
+				<p className="text-center text-lg text-muted-foreground">
+					This package uses the best of the NodeJS world under the hood.{' '}
+					<a href="https://discord.js.org" target="_blank" rel="noopener noreferrer">
+						Discord.js
+					</a>{' '}
+					is the most powerful library for creating bots and{' '}
+					<a href="https://nestjs.com" target="_blank" rel="noopener noreferrer">
+						Nest.js
+					</a>{' '}
+					is a progressive framework for creating well-architectured applications. This module
+					provides fast and easy way for creating Discord bots and deep integration with your
+					NestJS application.
 				</p>
+
+				<div className="flex flex-col items-center justify-center space-y-3">
+					<div className="flex flex-row space-x-4 h-fit">
+						<Button onClick={() => openLink('/introduction', true)}>Documentation</Button>
+
+						<Button
+							variant="outline"
+							onClick={() => openLink('https://discord.com/invite/mcBYvMTnwP')}
+						>
+							Community
+						</Button>
+
+						<Button
+							variant="outline"
+							onClick={() => openLink('https://github.com/necordjs')}
+						>
+							GitHub
+						</Button>
+					</div>
+
+					<small className="text-center text-muted-foreground text-xs">
+						or install it with package manager:
+					</small>
+
+					<NecordInstallScriptCopyBtn />
+				</div>
 			</div>
-		</Layout>
+		</>
 	);
 }
